dcffbcd3e1d401ce0b2feea6dc0b0f4f463c338c
Russell Greene <russellgreene8@gmail.com>
2017-11-04 13:16:09 -0600

    Add some basic docker images

---
+++ images/buidscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/chigraph/chigraph -b $CHI_REVISION --depth=1
+cd chigraph
+mkdir build && cd build
+cmake .. -DCMAKE_CXX_COMPILER="$CHI_COMPILER" -DCMAKE_BUILD_TYPE="$CHI_BUILD_TYPE" -DLLVM_CONFIG="$CHI_LLVM_CONFIG" -DCG_BUILD_DEBUGGER=OFF -GNinja
+ninja
+ninja test
+
